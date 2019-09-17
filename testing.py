7a38e3836266f986e8de42371716a3c039d7282b
Yehia Fahmy <yehia@Yehias-Pro.local>
2019-09-16 21:28:04 -0400

    updated testing

--- testing.py
+++ testing.py
@@ -1,8 +1,14 @@
-#for testing
+# for testing
 
 greeting = "Hello"
 name = "Bob"
 
-message = f'{greeting}, {name}. Welcome: '
+message = f"{greeting}, {name}. Welcome: "
 
 print(message)
+message = ""
+print("Input Your Name: ")
+
+input(message)
+print(f"Hello, {message}")
+
