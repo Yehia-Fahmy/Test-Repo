71bd3156fe290476a52008928999cc2b851e6baf
Yehia Fahmy <yehia@v1040-wn-rt-c-81-60.campus-dynamic.uwaterloo.ca>
2019-09-17 14:57:16 -0400

    finished functions

---
+++ Functions.py
@@ -0,0 +1,8 @@
+def my_function(name, age):
+    print("Your name is: %s" % (name))
+    print ("Your age is: %s" % (age))
+
+user_name = input("Input a Name: ")
+user_age = input("input an Age: ")
+
+my_function(user_name, user_age)
