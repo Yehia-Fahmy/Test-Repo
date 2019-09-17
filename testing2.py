920cf4540379d0ebfd8a3fc03f68a267b610e5df
Yehia Fahmy <yehia@Yehias-Pro.local>
2019-09-16 23:22:27 -0400

    learned about variable types

--- testing2.py
+++ testing2.py
@@ -1,6 +1,12 @@
-number = input("Input a Number")
-if number < 5:
-    print(f"{number} < 5")
-else:
-    print(f"{number} > 5")
+number = input("Input a Number: ")
+
+try:
+    value = int(number)
+    print("Yes the input is a number!")
+    if value < 5:
+        print(f"{value} < 5")
+    else:
+        print(f"{value} > 5")
+except ValueError:
+    print("No input is not a number :(")
 
