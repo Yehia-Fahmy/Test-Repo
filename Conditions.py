532456b400bb5b382725a82c6e303425d106c6f4
Yehia Fahmy <yehia@v1040-wn-rt-c-81-60.campus-dynamic.uwaterloo.ca>
2019-09-17 14:25:17 -0400

    adding some more tutorials

---
+++ Conditions.py
@@ -0,0 +1,6 @@
+name = input("What is your name: ")
+names = ["john", "adam", "conner"]
+if name in names:
+    print("Your name is recognized")
+else:
+    print("Your name is NOT recognized")
---
+++ Lists.py
@@ -0,0 +1,12 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+
+print(mylist[10])
